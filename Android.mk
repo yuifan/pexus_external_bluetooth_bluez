fd02db04b141cab40d483c0122162972bf382e6c
U-yuifan-PC\yuifan <yuifan@yuifan-PC.(none)>
2013-05-12 01:57:18 -0700

    update 4.2

--- Android.mk
+++ Android.mk
@@ -1,20 +1,16 @@
-#
-# Copyright (C) 2008 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-ifneq ($(TARGET_SIMULATOR),true)
-ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-  include $(all-subdir-makefiles)
+LOCAL_PATH := $(call my-dir)
+
+# Setup bdroid local make variables for handling configuration
+ifneq ($(BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR),)
+  bdroid_C_INCLUDES := $(BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR)
+  bdroid_CFLAGS := -DHAS_BDROID_BUILDCFG
+else
+  bdroid_C_INCLUDES :=
+  bdroid_CFLAGS := -DHAS_NO_BDROID_BUILDCFG
 endif
-endif
+
+include $(call all-subdir-makefiles)
+
+# Cleanup our locals
+bdroid_C_INCLUDES :=
+bdroid_CFLaGS :=
